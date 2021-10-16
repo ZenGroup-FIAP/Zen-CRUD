79c447df2f535844b754397c68bc0c9970e09105
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-10-16 00:27:42 -0300

    Inicialização do projeto, criação de classe de conexão, e credencials

---
+++ src/br/com/zenGroup/tools/ConnectionOracle.java
@@ -0,0 +1,41 @@
+package br.com.zenGroup.tools;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.zenGroup.credencials.Credencials;
+import oracle.jdbc.pool.OracleDataSource;
+
+public class ConnectionOracle {
+	private Connection connection;
+	private static ConnectionOracle instance = new ConnectionOracle();
+	
+	private ConnectionOracle() {
+		try {
+			OracleDataSource src = new OracleDataSource();
+			src.setURL(Credencials.STR_CONN);
+			src.setUser(Credencials.USER);
+			src.setPassword(Credencials.PWD);
+			connection = src.getConnection();
+		} catch (SQLException e) {
+			System.out.println("ERRO AO CONECTAR COM BANCO DE DADOS...");
+			System.out.println("VERIFICAR CREDENCIAIS!");
+			e.printStackTrace();
+		}
+	}
+	
+	public ConnectionOracle getConnection() {
+		return instance;
+	}
+	
+	public void closeConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			System.out.println("ERRO AO FECHAR CONEXÃO COM BANCO DE DADOS...");
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
