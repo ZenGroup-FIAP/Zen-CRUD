317d24e9c190df76bf6377bff78d2bd84db27ece
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-10-18 19:57:40 -0300

    Finalização da entidade paciente

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,17 @@
 package application;
 
-import java.sql.Date;
 import java.sql.SQLException;
 
-import br.com.zenGroup.to.PacienteTO;
+import br.com.zenGroup.bo.PacienteBO;
 
 public class Program {
 	public static void main(String[] args) throws SQLException {
-		PacienteTO paciente = new PacienteTO();
-		paciente.setCodigo(1);
-		paciente.setCpf("510.605.523-88");
-		paciente.setEmail("sla@email.com");
-		paciente.setNascimento(new Date(0));
-		paciente.setNome("Jorge");
-		paciente.setSenha("1234");
-		paciente.setTelefone("119565659");
+		// Testes
+		PacienteBO bo = new PacienteBO();
 		
+		// Lista de pacientes
+		System.out.println(bo.listar());
 		
-		System.out.println(paciente);
+		// Cadastrar paciente
 	}
 }
